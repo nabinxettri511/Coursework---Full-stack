ec713d1d4255f4600dcd79da505b35274d8732fc
nabinxettri511 <nabinxettri2004@gmail.com>
2026-04-03 11:26:42 +0100

    Add files via upload

---
+++ add-car-form.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+  </head>
+  <body>
+    <div class="container">
+      <h1>Add a Car</h1>
+      <form action="add-car.php" method="post">
+        <div class="mb-3">
+          <label for="CarName" class="form-label">Car name</label>
+          <input type="text" class="form-control" id="CarName" name="CarName">
+        </div>
+        <div class="mb-3">
+          <label for="CarDescription" class="form-label">Description</label>
+          <textarea class="form-control" id="CarDescription" name="CarDescription" rows="5"></textarea>
+        </div>
+        <div class="mb-3">
+          <label for="DateEstablished" class="form-label">Date established</label>
+          <input type="date" class="form-control" id="DateEstablished" name="DateEstablished">
+        </div>  
+        <div class="mb-3">
+          <label for="CarRating" class="form-label">Rating</label>
+          <input type="number" class="form-control" id="CarRating" name="CarRating">
+        </div>         
+        <input type="submit" class="btn btn-primary" value="Add car">
+      </form>
+    </div>
+  </body>
+</html>	      
